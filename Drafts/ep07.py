00cc1366c35177b9c922917614a46bec3c068d49
DESKTOP-MV9IIF6\June <june.antonsen@gmail.com>
2022-06-24 03:24:09 +0200

    improved

--- Drafts/ep07.py
+++ Drafts/ep07.py
@@ -1,13 +1,11 @@
 #By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 #What is the 10 001st prime number?
 nth = 10001
-num = 1
-c = 1
+c = 0
 i = 1
-while c < nth + 1:
-  num = i
+while c < nth:
   i += 1
-  if not any(i % n == 0 for n in range(2, num)): #if num not divideable by any number between 2 and itself.
+  if not any(i % n == 0 for n in range(2, i)): #if num not divideable by any number between 2 and itself.
      c += 1
 
 print(i)
