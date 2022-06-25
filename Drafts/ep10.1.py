3278a8589419646e665e6fcddabad5d1c447a313
DESKTOP-MV9IIF6\June <june.antonsen@gmail.com>
2022-06-25 22:58:47 +0200

    update ep10.1

--- Drafts/ep10.1.py
+++ Drafts/ep10.1.py
@@ -1,22 +1,10 @@
 #Extension. See ep10.py for problem description
 #testing examplecode from https://switowski.com/blog/for-loop-vs-list-comprehension
 
-MILLION_NUMBERS = list(range(2,2))
+MILLION_NUMBERS = list(range(2,2_000_000))
 
-def transform(number):
-    if any(number % n == 0 for n in range(2, number)):
-       number = 0
-    return number
+blabla = [number for number in MILLION_NUMBERS if not any(number % n == 0 for n in range(2, number))]
 
-def fizz_buzz2():
-    output = 0
-    for number in MILLION_NUMBERS:
-        output += transform(number)
-    return output
+print(sum(blabla))
 
-s = fizz_buzz2()
-print(s)
-#def fizz_buzz2_comprehension():
-#    return [transform(number) for number in MILLION_NUMBERS]
-#s = fizz_buzz2_comprehension()
-#print(s)
+#this also doesn't work with the big range...
