0e7bdaae62e0bb19caa7c9e386db98d9d3310d8b
DESKTOP-MV9IIF6\June <june.antonsen@gmail.com>
2022-06-25 05:17:32 +0200

    comment

--- Drafts/ep10.py
+++ Drafts/ep10.py
@@ -1,15 +1,16 @@
 #The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 #Find the sum of all the primes below two million.
 
-num = 10
+num = 2000000
 tot = 0
 i = 1
-for i < num:
+for i in range (1,num):
   i += 1
   if not any(i % n == 0 for n in range(2, i)): #if num not divideable by any number between 2 and itself.
      tot += i
 print(tot)
 
 #Process: quick and easy reuse of ep07, works fine whith the example num = 10,
-#BUT - doesnt work with two million, the program kept working for two minutes before I aborted.
-#Went searching for python memory problem while looping...
+#BUT - doesnt work with two million, the program kept looping for two minutes before I aborted.
+#Went searching online for python looping-advice... and I'm not sure this loop-method
+#is the way to go about it.
