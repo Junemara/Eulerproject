8472ec809a4f0cd5e6dc6199fa44b60d14c9301f
DESKTOP-MV9IIF6\June <june.antonsen@gmail.com>
2022-06-29 04:40:24 +0200

    improved

--- Drafts/ep10.2.py
+++ Drafts/ep10.2.py
@@ -1,30 +1,22 @@
 #Extension #2. See ep10.py for problem description
 #See ep10.1.py for context
 
-import numpy as np
-from functools import reduce
-from operator import add
+n = 2000000
+import numpy
+def primesfrom3to(n):
+    #""" Returns a array of primes, 3 <= p < n """
+    sieve = numpy.ones(n//2, dtype=bool)
+    for i in range(3,int(n**0.5)+1,2):
+        if sieve[i//2]:
+            sieve[i*i//2::i] = False
+    return sum(2*numpy.nonzero(sieve)[0][1::]+1)
 
+s = primesfrom3to(n) + 2
+print(s)
 
-
-MILLION_NUMBERS = list(range(2,10))
-
-filtered = filter(lambda number: not any(number % n == 0 for n in range(2, number)), MILLION_NUMBERS)
-#mja = reduce(lambda x,y: x+y, filtered)
-
-print(filtered)
-
-#hehe = reduce(lambda a,d: 10*a+d, [1,2,3,4,5,6,7,8], 0)
-
-#a = np.array(filtered)
-#print(filtered.cumsum())
-#blabla = [number for number in MILLION_NUMBERS if not any(number % n == 0 for n in range(2, number))]
-
-#reduce(mul,blabla)
-
-#print(reduce(add,filtered))
-
-#Process: this also doesn't work with the big range...
-#UNTIL I introduced the reduce function, and it still doesn't work...
 #Some hours later... I have messed around with lambda, filter and a grotesque mixture of inspiration
 #from forums and blogs discussing looping with large lists in python.
+#Finally a code that works. Sadly it was copy-pasted and barely edited from
+#https://stackoverflow.com/questions/2068372/fastest-way-to-list-all-primes-below-n
+#Thanks to Robert William Hanks(which is not his real name) on stockoverflow- I can move on from this
+#frustrating almost-got-it problem, and hopefully get back to it later.
