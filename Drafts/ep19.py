84868e264e1aa64b0ae858ff7290c44fbe7d8fad
DESKTOP-MV9IIF6\June <june.antonsen@gmail.com>
2022-07-04 09:50:27 +0200

    update

--- Drafts/ep19.py
+++ Drafts/ep19.py
@@ -9,15 +9,37 @@
 #A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
 #How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
 
-week = [mon, tues, wed, thur, fri, sat, sun]
-month = [jan, feb, mar, apr, may, jun, jul, aug, sept, oct, nov, dec]
-day30 = [apr, jun, sept, nov]
-day31 = [jan, mar, apr, may, jul, aug, oct, dec]
+week = ['mon', 'tues', 'wed', 'thur', 'fri', 'sat', 'sun']
+month = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sept', 'oct', 'nov', 'dec']
+day30 = ['apr', 'jun', 'sept', 'nov']
+day31 = ['jan', 'mar', 'apr', 'may', 'jul', 'aug', 'oct', 'dec']
+feb = 28
+
+e = 'feb'
+fromday = 0
+frommonth = 0
+fromyear = 1901
+
+def daymonth(e):
+    d = 0
+    if e in day30:
+      d = 30
+    if e in day31:
+      d = 31
+    else:
+      d = feb
+    return d
+
+def letsgo():
+    year = 365
+    #decision to postpone this problem was made here
+    for i in range(0,year+1):
+      fromday += 1
 
 
-def year():
 
-
+f = daymonth(e)
+print(f)
 
 
 #Process: reading the problem description is a promlem in itself. Would have stabbed caesar myself tbh.
