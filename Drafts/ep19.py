23cb5b148345445c6ce600b9df6f847349fd15a3
DESKTOP-MV9IIF6\June <june.antonsen@gmail.com>
2022-07-04 09:15:11 +0200

    add ep19

---
+++ Drafts/ep19.py
@@ -0,0 +1,24 @@
+#You are given the following information, but you may prefer to do some research for yourself.
+#1 Jan 1900 was a Monday.
+#Thirty days has September,
+#April, June and November.
+#All the rest have thirty-one,
+#Saving February alone,
+#Which has twenty-eight, rain or shine.
+#And on leap years, twenty-nine.
+#A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
+#How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+
+week = [mon, tues, wed, thur, fri, sat, sun]
+month = [jan, feb, mar, apr, may, jun, jul, aug, sept, oct, nov, dec]
+day30 = [apr, jun, sept, nov]
+day31 = [jan, mar, apr, may, jul, aug, oct, dec]
+
+
+def year():
+
+
+
+
+#Process: reading the problem description is a promlem in itself. Would have stabbed caesar myself tbh.
+#Disgusting.
