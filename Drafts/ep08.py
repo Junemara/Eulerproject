e88f4e2a403da7cf564f91f58cf287a8ef20c54f
DESKTOP-MV9IIF6\June <june.antonsen@gmail.com>
2022-07-10 00:32:24 +0200

    ep08.py code works

--- Drafts/ep08.py
+++ Drafts/ep08.py
@@ -2,4 +2,38 @@
 #Find the thirteen adjacent digits in the 1000-digit number that have the greatest product.
 #What is the value of this product?
 
-#wtf
+from functools import reduce
+from operator import mul
+
+def findp(filename):
+    infile = open(filename)
+    nums = []
+    for e in infile:
+      temp = e.replace("\n","")
+      for i in temp:
+        i = int(i)
+        nums.append(i)
+    infile.close()
+    idx = 0
+    bmax = 0
+    bitmax = 0
+    for i in nums:
+        bit = nums[idx:idx+13]
+        prod = reduce(mul, bit)
+        idx += 1
+        if prod > bmax:
+            bmax = prod
+            bitmax = bit
+    return bmax, bitmax
+
+res = findp("Drafts\ep08input.txt")
+print(res)
+
+#Process: Had a really hard time getting the problemdescription, something about the word "adjacent"
+#was confusing, + didn't  really get what the problem was asking before doing some other problems and
+#getting familiar with how problems are set up with input etc.
+
+#Currently in shock by getting the right answer checking with projecteuler.
+#The whole function was written without testing, only looking to ep22 for the inputfile code.
+#Was prepared for a list of errors to correct, and to scratch my brain a bit before backtracking
+#and fixing everything. But it just works. Straight from my brain. Amazing.
